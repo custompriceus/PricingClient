9bbc6cf2fe5738ffd7b989d5184f577df406e5a0
Jonathan Weinstein <jweinst4@gmail.com>
2024-07-15 17:07:03 -0400

    cors fix

--- src/resources/api.js
+++ src/resources/api.js
@@ -21,12 +21,6 @@ export function getShirtPricingDisplay() {
     return new Promise((resolve, reject) => {
         axios({
             url: url + "api/user/getShirtPricingDisplay",
-            headers: {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
-                "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length"
-            }, 
             method: "GET"
         })
             .then(res => {
