cfc744ebc51eb8c750b6a0b0a6d5a3f5e86da6b9
itsandeepv <itsandeepverma@gmail.com>
2025-07-16 22:53:31 +0530

    css issue fix done

--- src/routes/index.js
+++ src/routes/index.js
@@ -58,6 +58,7 @@ function Routes() {
                         <LoginComponent />
                     )}
                 />
+                
                 <Route
                     exact
                     path={"/"}
